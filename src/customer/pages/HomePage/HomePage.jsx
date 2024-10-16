2122b9ffb5d486a61bc20784b692204425be99d8
aryanagn <aryanbhat87@gmail.com>
2024-10-15 17:32:04 -0700

    added nav bar

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainCarousel/>
+        <div>
+            other section
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
